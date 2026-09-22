7d65f3962bfffe5111a5f6de1ca6787d2095d7f5
root <root@Ryo-PC.localdomain>
2026-09-22 17:05:41 +0900

    fix-プロフィール画像の問題切り分け3

--- src/main/java/com/spring/springbootapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/springbootapplication/exception/GlobalExceptionHandler.java
@@ -14,16 +14,13 @@ public class GlobalExceptionHandler {
   }
 
   ExceptionHandler(Exception.class)
-public String handleGeneral(Exception e, Model model) {
-
-  System.out.println("=== 予期しない例外 ===");
-  e.printStackTrace();
-
-  model.addAttribute(
-    "errorMessage",
-    "予期しないエラーが発生しました"
-  );
-
+  public String handleGeneral(Exception e, Model model) {
+    System.out.println("=== 予期しない例外 ===");
+    e.printStackTrace();
+    model.addAttribute(
+      "errorMessage",
+      "予期しないエラーが発生しました"
+    );
   return "error";
-}
+  }
 }
